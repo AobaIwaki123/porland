59eb19f31e6a5043282526c570dcb02992873e36
AobaIwaki <aobaiwaki@gmail.com>
2025-12-02 17:45:02 +0900

    add: porland記法

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-def porland(A: list[int]):
-    print(A)
+def porland(S: str):
+    stack = []
+    ans = 0
+    for c in S:
+        if c in "+-*/":
+            print("calc")
+            match c:
+                case "+":
+                    ans = int(stack[-1]) + int(stack[-2])
+                case "-":
+                    ans = int(stack[-1]) - int(stack[-2])
+                case "/":
+                    ans = int(stack[-1]) / int(stack[-2])
+                case "*":
+                    ans = int(stack[-1]) * int(stack[-2])
+            stack.pop(-1)
+            stack.pop(-1)
+        else:
+            stack.append(c)
+        print(stack)
+    if (len(stack) > 0):
+        raise ValueError("Stack Has Value", stack)
+    print("Answer:", ans)
 
 def main():
-    print("Hello from porland!")
+    A = "11+"
+    porland(A)
 
 
 if __name__ == "__main__":
